63ee8aa96caaa4e404ae689b1b33234e75a000b2
g-kisenkov <85692332+g-kisenkov@users.noreply.github.com>
2022-03-25 11:29:36 +0100

    Updated docs; Removed hardcoded values; Reformatted the source;

--- cdk-ec2-instagram/lib/cdk-ec2-instagram-stack.ts
+++ cdk-ec2-instagram/lib/cdk-ec2-instagram-stack.ts
@@ -1,63 +1,80 @@
 import * as ec2 from "aws-cdk-lib/aws-ec2";
-import * as cdk from 'aws-cdk-lib';
-import * as iam from 'aws-cdk-lib/aws-iam'
-import { KeyPair } from 'cdk-ec2-key-pair';
-import * as path from 'path';
-import { Asset } from 'aws-cdk-lib/aws-s3-assets';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { KeyPair } from "cdk-ec2-key-pair";
+import * as path from "path";
+import { Asset } from "aws-cdk-lib/aws-s3-assets";
+import { Construct } from "constructs";
 
 export class CdkEc2InstagramStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-        // Create the Key Pair
-    const key = new KeyPair(this, 'A-Key-Pair', {
-      name: 'cdk-ec2-instagram',
-      description: 'This is a Key Pair for EC2 instance',
-      storePublicKey: true
+    // Create the Key Pair
+    const key = new KeyPair(this, "A-Key-Pair", {
+      name: "cdk-ec2-instagram",
+      description: "This is a Key Pair for EC2 instance",
+      storePublicKey: true,
     });
 
-    const vpc = new ec2.Vpc(this, 'VPC', {
+    const vpc = new ec2.Vpc(this, "VPC", {
       natGateways: 0,
-      subnetConfiguration: [{
-        cidrMask: 24,
-        name: "insta_subnet",
-        subnetType: ec2.SubnetType.PUBLIC
-      }]
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "insta_subnet",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
     });
 
-    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+    const securityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
       vpc,
-      description: 'Allow SSH (TCP port 22) in',
-      allowAllOutbound: true
+      description: "Allow SSH (TCP port 22) in",
+      allowAllOutbound: true,
     });
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH Access')
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.allTcp(), 'Allow HTTP Access')
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      "Allow SSH Access"
+    );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.allTcp(),
+      "Allow HTTP Access"
+    );
 
-    const role = new iam.Role(this, 'ec2Role', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
-    })
+    const role = new iam.Role(this, "ec2Role", {
+      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
+    });
 
     // Grant read access to the private key to a role or user
-    key.grantReadOnPrivateKey(role)
+    key.grantReadOnPrivateKey(role);
 
-    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'))
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
+    );
 
     const ami = new ec2.AmazonLinuxImage({
       generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      cpuType: ec2.AmazonLinuxCpuType.X86_64
+      cpuType: ec2.AmazonLinuxCpuType.X86_64,
     });
 
-    const ec2Instance = new ec2.Instance(this, 'Instance', {
+    const ec2Instance = new ec2.Instance(this, "Instance", {
       vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
       machineImage: ami,
       securityGroup: securityGroup,
       keyName: key.keyPairName,
-      role: role
+      role: role,
     });
 
-    const asset = new Asset(this, 'Asset', { path: path.join(__dirname, '../configuration/config.sh') });
+    const asset = new Asset(this, "Asset", {
+      path: path.join(__dirname, "../configuration/config.sh"),
+    });
     const localPath = ec2Instance.userData.addS3DownloadCommand({
       bucket: asset.bucket,
       bucketKey: asset.s3ObjectKey,
@@ -65,11 +82,15 @@ export class CdkEc2InstagramStack extends cdk.Stack {
 
     ec2Instance.userData.addExecuteFileCommand({
       filePath: localPath,
-      arguments: '--verbose -y'
+      arguments: "--verbose -y",
     });
     asset.grantRead(ec2Instance.role);
 
-    new cdk.CfnOutput(this, 'IP Address', { value: ec2Instance.instancePublicIp });
-    new cdk.CfnOutput(this, 'ssh command', { value: 'ssh -i cdk-ec2-instagram.pem -o IdentitiesOnly=yes ec2-user@' + ec2Instance.instancePublicIp })
+    new cdk.CfnOutput(this, "IP Address", {
+      value: ec2Instance.instancePublicIp,
+    });
+    new cdk.CfnOutput(this, "ssh command", {
+      value: `ssh -i ${key.keyPairName} -o IdentitiesOnly=yes ec2-user@ ${ec2Instance.instancePublicIp}`,
+    });
   }
 }
