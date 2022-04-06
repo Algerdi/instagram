6249d955b4b2867bc9cdab4b26f20886e0302e68
g-kisenkov <85692332+g-kisenkov@users.noreply.github.com>
2022-04-07 00:41:30 +0200

    Fishtank feature
    The so-much-awaited feature is finally out!

--- ui/web/src/index.tsx
+++ ui/web/src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Fishtank from './routes/fishtank'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/fishtank" element={<Fishtank />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
